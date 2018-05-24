503d38f6c51e6955f567e24eecb5a7bbcb6b9971
unknown <paulokowalski@gmail.com>
2018-05-23 23:49:29 -0300

    Alguns ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
+import { browser } from 'protractor';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { DataTableModule } from 'primeng/datatable';
 import { TooltipModule } from 'primeng/tooltip';
 import { DropdownModule } from 'primeng/dropdown';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { CalendarModule } from 'primeng/calendar';
 
 import { AppComponent } from './app.component';
 import { MensalidadePesquisaComponent } from './mensalidade-pesquisa/mensalidade-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PessoaPesquisaComponent } from './pessoa-pesquisa/pessoa-pesquisa.component';
 import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  {path: '', component: HomeComponent},
   {path: 'mensalidades', component: MensalidadePesquisaComponent},
   {path: 'pessoas', component: PessoaPesquisaComponent},
   {path: 'pessoas/novo', component: PessoaCadastroComponent}
@@ -29,7 +35,8 @@ const routes: Routes = [
     MensalidadePesquisaComponent,
     NavbarComponent,
     PessoaPesquisaComponent,
-    PessoaCadastroComponent
+    PessoaCadastroComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -39,7 +46,10 @@ const routes: Routes = [
     TooltipModule,
     FormsModule,
     DropdownModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    InputSwitchModule,
+    CalendarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
